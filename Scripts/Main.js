12c23a5e6df580fe16ebc86ebd9261caa5a3b8e8
unknown <nourhalhoul@gmail.com>
2023-03-09 18:46:37 -0800

    Fixing after Google update

--- Scripts/Main.js
+++ Scripts/Main.js
@@ -1,12 +1,9 @@
-var link = document.querySelectorAll("a")
-
-for (let i = 0; i < link.length; i++)
-{
-    link[i].href.includes(",vid:") ? Redirect(link[i]) : null;
-}
-
-function Redirect(link)
-{
+let links = document.querySelectorAll('a');
+for (let i = 0, len = links.length; i < len; i++) {
+  let link = links[i];
+  link.removeAttribute('data-vll');
+  if (link.href.includes(",vid:")) {
     let splitURL = link.href.split(',vid:', 2);
     link.href = 'https://www.youtube.com/watch?v=' + splitURL[1];
+  }
 }
