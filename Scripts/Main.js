d35227b88798ed5ff84e2cfb3aa38321a92f186d
Nour <nourhalhoul@gmail.com>
2024-04-07 10:15:37 -0400

    Run Quora-remover only from the All tab

--- Scripts/Main.js
+++ Scripts/Main.js
@@ -1,10 +1,12 @@
 let enableQuora = false;
 let enableYoutube = false;
+let enableReddit = false;
 
-chrome.storage.local.get(['quoraState', 'youtubeState'], function (result)
+chrome.storage.local.get(['quoraState', 'youtubeState', 'redditState'], function (result)
 {
   enableQuora = result.quoraState || false;
   enableYoutube = result.youtubeState || false;
+  enableReddit = result.redditState || false;
   setFeatures(); // Run with initial state
 });
 
@@ -22,11 +24,16 @@ chrome.storage.onChanged.addListener(function (changes, namespace)
       enableYoutube = newValue;
       console.log("YouTube is " + (enableYoutube ? "enabled" : "disabled"));
     }
+    if (key === 'redditState')
+    {
+      enableReddit = newValue;
+      console.log("Reddit is " + (enableReddit ? "enabled" : "disabled"));
+    }
   }
   setFeatures();
 });
 
-
+// Enable features
 function setFeatures()
 {
   if (enableQuora)
@@ -37,21 +44,28 @@ function setFeatures()
   {
     runYoutube()
   }
+  if (enableReddit)
+  {
+    runReddit()
+  }
 }
 
+// Run features
 function runQuora()
 {
-  let results = document.querySelectorAll('span');
-
-  results.forEach(result =>
+  if (document.querySelector('.YmvwI[selected]').textContent == 'All')
   {
-    if (result.textContent === 'Quora')
-    {
-      let grandparent = result.closest('.MjjYud');
-      if (grandparent) grandparent.remove();
-    }
-  })
+    let results = document.querySelectorAll('span');
 
+    results.forEach(result =>
+    {
+      if (result.textContent === 'Quora')
+      {
+        let grandparent = result.closest('.MjjYud');
+        if (grandparent) grandparent.remove();
+      }
+    })
+  }
 }
 
 function runYoutube()
@@ -73,6 +87,50 @@ function runYoutube()
   });
 }
 
+function runReddit()
+{
+  // Get original search button
+  const originalSearchButton = document.querySelector('button.Tg7LZd');
+
+  // Clone search button
+  const clonedSearchButton = originalSearchButton.cloneNode(true);
+
+  // Remove cloned searched button's icon
+  const oldIcon = clonedSearchButton.querySelector('svg');
+  if (oldIcon)
+  {
+    oldIcon.parentNode.removeChild(oldIcon);
+  }
+
+  // Add Reddit icon to the cloned searched button
+  const redditIco = document.createElement('img');
+  redditIco.src = chrome.runtime.getURL('Images/reddit.png');
+  redditIco.style.width = '24px';
+  redditIco.style.height = '24px';
+
+  // Append to a span element
+  const spanElement = clonedSearchButton.querySelector('.z1asCe.MZy1Rb');
+  if (spanElement)
+  {
+    spanElement.appendChild(redditIco);
+  } else
+  {
+    // If the span does not exist, append the icon directly to the button
+    clonedSearchButton.appendChild(redditIco);
+  }
+
+  // Append " reddit" and search when clicked
+  clonedSearchButton.addEventListener('click', () =>
+  {
+    document.querySelector('textarea.gLFyf[aria-label="Search"][aria-owns="Alh6id"]').value = document.querySelector('textarea.gLFyf[aria-label="Search"][aria-owns="Alh6id"]').value + ' reddit'
+
+  }, true)
+
+  // Append to parent container and align it next to the search button
+  originalSearchButton.parentNode.insertBefore(clonedSearchButton, originalSearchButton.nextSibling);
+
+}
+
 
 const observer = new MutationObserver((mutationsList) =>
 {
@@ -80,7 +138,8 @@ const observer = new MutationObserver((mutationsList) =>
   {
     if (mutation.type === 'childList')
     {
-      setFeatures();
+      runYoutube();
+      runQuora();
     }
   }
 });
@@ -90,6 +149,3 @@ if (targetNode)
 {
   observer.observe(targetNode, { childList: true, subtree: true });
 }
-
-// Initial run
-setFeatures();
--- popup.js
+++ popup.js
@@ -2,11 +2,13 @@ document.addEventListener('DOMContentLoaded', function ()
 {
     var quora = document.getElementById('quora');
     var youtube = document.getElementById('youtube');
+    var reddit = document.getElementById('reddit');
 
-    chrome.storage.local.get(['quoraState', 'youtubeState'], function (result)
+    chrome.storage.local.get(['quoraState', 'youtubeState', 'redditState'], function (result)
     {
         quora.checked = result.quoraState || false;
         youtube.checked = result.youtubeState || false;
+        reddit.checked = result.redditState || false;
     });
 
     quora.addEventListener('change', function ()
@@ -17,5 +19,10 @@ document.addEventListener('DOMContentLoaded', function ()
     youtube.addEventListener('change', function ()
     {
         chrome.storage.local.set({ 'youtubeState': youtube.checked });
+    });
+
+    reddit.addEventListener('change', function ()
+    {
+        chrome.storage.local.set({ 'redditState': reddit.checked });
     });
 });
